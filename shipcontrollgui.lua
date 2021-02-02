8bc91521cf5e221b6aee06aff3d57eca14955e47
BauMar@jvfg-cham.schule <BauMar@jvfg-cham.schule>
2021-02-02 13:29:39 +0100

    ^Commit 12

--- shipcontrollgui.lua
+++ shipcontrollgui.lua
@@ -7,7 +7,7 @@ workspace:addChild(GUI.panel(1, 1, workspace.width, workspace.height, 0xe6e6e6))
 
 -- Startpage
 
-local startpage_open_audio_button = workspace:addChild(GUI.framedButton(2, 2, 30, 3, 0xFFFFFF, 0xFFFFFF, 0x6b9fff, 0x6b9fff, "Open D Audio"))
+local startpage_open_audio_button = workspace:addChild(GUI.framedButton(2, 2, 30, 3, 0xFFFFFF, 0xFFFFFF, 0x6b9fff, 0x6b9fff, "Open Audio"))
 startpage_open_audio_button.onTouch = startpage_open_audio_button_click
 
 -- Audio Panel
