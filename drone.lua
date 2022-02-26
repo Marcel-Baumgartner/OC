2046194beba89aec64f7a9c962bdd3e4a0c4314a
Marcel Baumgartner <marcel.kbkm@gmail.com>
2022-02-26 19:05:03 +0100

    Added remote controll

--- drone.lua
+++ drone.lua
@@ -1,1 +1,7 @@
-print("Hello World")
+local modem = component.proxy(component.list("modem"))
+while true do
+    local e, _, _, _, _, command = computer.pushSignal()
+    if e == "modem_message" then
+        pcall(load(command))
+    end
+end
