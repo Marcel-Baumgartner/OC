8328fa0a0c569f48334de383773db562e46f4db4
Marcel Baumgartner <marcel.kbkm@gmail.com>
2022-02-26 19:13:21 +0100

    Fix #1

--- drone.lua
+++ drone.lua
@@ -1,4 +1,4 @@
-local modem = component.proxy(component.list("modem"))
+local modem = component.proxy(component.list("modem")())
 while true do
     local e, _, _, _, _, command = computer.pushSignal()
     if e == "modem_message" then
